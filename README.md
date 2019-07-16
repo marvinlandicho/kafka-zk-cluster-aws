693625b3263ee4f3fce9aa1655942c6f630ab452
marvinlandicho <marvinlandicho@mnlandicho-2.local>
2019-07-16 17:05:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#### Description
+
+Module that creates a Kafka cluster and zookeeper cluster.
+Also requires a module that created cluster subnets.
---
+++ modules.tf
@@ -0,0 +1,30 @@
+module "zookeeper-cluster" {
+  source      = "github.com/terraform-kafka/terraform-aws-zookeeper"
+  name        = "zk"
+  cnt         = 3
+  subnet_ids  = "${module.zookeeper-subnets.subnet_ids}"
+  subnet_cidr = "${module.zookeeper-subnets.subnet_cidr}"
+  subnet_az   = "${module.zookeeper-subnets.subnet_az}"
+  ami         = "ami-07f02e8be5da14095"
+}
+
+module "kafka-cluster" {
+  source      = "github.com/marvinlandicho/terraform-aws-kafka"
+  name        = "kf"
+  cnt         = 3
+  subnet_ids  = "${module.zookeeper-subnets.subnet_ids}"
+  subnet_cidr = "${module.zookeeper-subnets.subnet_cidr}"
+  subnet_az   = "${module.zookeeper-subnets.subnet_az}"
+  ami         = "ami-041c154f9636c34ce"
+}
+
+module "zookeeper-subnets" {
+  source      = "github.com/terraform-kafka/terraform-aws-multi-az-subnets"
+  name        = "broker-zookeeper-group"
+  vpc_id      = "vpc-0d2fe90286b6e30ea"
+  cidr_blocks = [
+    "10.0.11.0/24",
+    "10.0.12.0/24",
+    "10.0.13.0/24"
+  ]
+}
